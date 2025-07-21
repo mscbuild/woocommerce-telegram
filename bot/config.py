cab36ff2c656d777b2347a58e8aa4890b1b4b3a1
JR <180821520+mscbuild@users.noreply.github.com>
2025-07-21 20:11:16 +0300

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,8 @@
+# Telegram
+TELEGRAM_BOT_TOKEN = "your_telegram_bot_token"
+TELEGRAM_CHAT_ID = "your_chat_id"  # Can be user ID or group ID
+
+# WooCommerce
+WC_URL = "https://yourstore.com"
+WC_KEY = "ck_xxx"
+WC_SECRET = "cs_xxx"
